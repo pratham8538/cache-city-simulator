9e8daa2d451333d58bbe3453fc401c3a0b370364
Pratham M Patel <24CS070@Charusat.edu.in>
2026-03-31 23:04:32 +0530

    Deploy to GitHub Pages

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ mode }) => {
+	const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+	const base = mode === 'production' && repoName ? `/${repoName}/` : '/';
+
+	return {
+		base,
+	};
+});
